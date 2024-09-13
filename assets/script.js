085430fba4675701fa16edf1fd2796878ec78b27
James Thomas <j.thomas@bristol.ac.uk>
2024-09-13 17:05:56 +0100

    Improve scroll behaviour of header and filter form

--- assets/script.js
+++ assets/script.js
@@ -36,17 +36,24 @@ document.addEventListener("DOMContentLoaded", () => {
             let no_results = section.querySelectorAll("& > li:not([hidden])").length === 0;
             section.closest("section").classList.toggle("no-results", no_results);
         }
+
+        // Scroll to the top of the course directory, but because it is sticky we scroll
+        // to the end of the previous element
+        let previous_element = document.querySelector("#course-filters").previousElementSibling
+        document.documentElement.scrollTo({ top: previous_element.offsetTop + previous_element.offsetHeight + parseFloat(window.getComputedStyle(previous_element).marginBottom), behavior: "smooth" });
     }
 
     // Add category and free-text filters below the course directory TOC
     let filters = document.querySelector("#course-filters");
     filters.innerHTML = `
-        <label for="text-filter">Search for course: <input type="text" id="text-filter" class="form-control" placeholder="Enter search text" size="30"></label>
-        <label for="category-filter">Filter by category: <select id="category-filter" class="form-select">
-            <option value="">All</option>
-            ${Array.from(categories).map(category => `<option value="${category}">${category}</option>`).join("")}
-        </select></label>
-        <button id="reset-filters" class="btn btn-light">Reset filters</button>
+        <div>
+            <label for="text-filter">Search for course: <input type="text" id="text-filter" class="form-control" placeholder="Enter search text" size="30"></label>
+            <label for="category-filter">Filter by category: <select id="category-filter" class="form-select">
+                <option value="">All</option>
+                ${Array.from(categories).map(category => `<option value="${category}">${category}</option>`).join("")}
+            </select></label>
+            <button id="reset-filters" class="btn btn-light">Reset filters</button>
+        </div>
     `;
     document.querySelector("#text-filter").addEventListener("input", filter_courses);
     document.querySelector("#category-filter").addEventListener("change", filter_courses);
