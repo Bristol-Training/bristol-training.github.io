d2771b63ca447eb622fe781a274f529108594dfe
James Thomas <j.thomas@bristol.ac.uk>
2024-09-13 17:47:38 +0100

    Improve search UI when no results found

--- assets/script.js
+++ assets/script.js
@@ -11,8 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let categories = new Set([...category_order, ...used_categories].filter(category => used_categories.has(category)));
 
     /**
-     * Filter the courses by the category and free-text filters, then filter out whole sections
-     * if they contain no visible courses
+     * Filter the courses by the category and free-text filters and update the UI
      */
     function filter_courses() {
         let selected_category = document.querySelector("#category-filter").value;
@@ -37,6 +36,12 @@ document.addEventListener("DOMContentLoaded", () => {
             section.closest("section").classList.toggle("no-results", no_results);
         }
 
+        // The search results summary should be shown if there are any filters applied
+        let results_shown = document.querySelectorAll("ul.course-listing > li:not([hidden])").length;
+        document.querySelector("#courses-shown").innerText = results_shown;
+        document.querySelector("#course-results-summary").hidden = !(selected_category || filter_text);
+        document.querySelector("#course-zero-results").hidden = results_shown;
+
         // Scroll to the top of the course directory, but because it is sticky we scroll
         // to the end of the previous element
         let previous_element = document.querySelector("#course-filters").previousElementSibling
@@ -46,14 +51,15 @@ document.addEventListener("DOMContentLoaded", () => {
     // Add category and free-text filters below the course directory TOC
     let filters = document.querySelector("#course-filters");
     filters.innerHTML = `
-        <div>
-            <label for="text-filter">Search for course: <input type="text" id="text-filter" class="form-control" placeholder="Enter search text" size="30"></label>
-            <label for="category-filter">Filter by category: <select id="category-filter" class="form-select">
-                <option value="">All</option>
-                ${Array.from(categories).map(category => `<option value="${category}">${category}</option>`).join("")}
-            </select></label>
-            <button id="reset-filters" class="btn btn-light">Reset filters</button>
+        <label for="text-filter">Search for course: <input type="text" id="text-filter" class="form-control" placeholder="Enter search text" size="30" autocomplete="off"></label>
+        <label for="category-filter">Filter by category: <select id="category-filter" class="form-select">
+            <option value="">All</option>
+            ${Array.from(categories).map(category => `<option value="${category}">${category}</option>`).join("")}
+        </select></label>
+        <div id="course-results-summary" hidden>
+            Currently showing <span id="courses-shown"></span> out of ${courses.length} total courses. <button id="reset-filters" class="btn btn-light">Reset filters</button>
         </div>
+        <div id="course-zero-results" hidden>No courses matched your search.</div>
     `;
     document.querySelector("#text-filter").addEventListener("input", filter_courses);
     document.querySelector("#category-filter").addEventListener("change", filter_courses);
